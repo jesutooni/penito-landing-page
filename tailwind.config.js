4071a1729aae360d6585f32697b1544d3accdbbf
Jesutooni <edeworuyoyou@gmail.com>
2022-04-14 11:35:23 +0100

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        penito: {
+          black: "#122025",
+          white: "#F8FBFB",
+          gray: "#58666B",
+          yellow: "#F2C229",
+          "yellow-light": "#FDD835",
+          blue: "#0EBFDD",
+          "blue-dark": "#1C2A2F",
+          pink: "#FF89A8",
+          orange: "#FB7844",
+          green: "#4EA00B",
+          "green-light": "#1FB890",
+        },
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};

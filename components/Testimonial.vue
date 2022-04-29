339c49bfc3c5cc8cb751e0b97ef85a92bc00dd93
Jesutooni <edeworuyoyou@gmail.com>
2022-04-29 13:43:34 +0100

    testimonials section

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="bg-penito-gray-bg py-16">
-    <div class="max-w-6xl mx-auto">
-      <h1 class="text-[1.75rem] text-penito-black font-semibold">
-        Our Customer Say
-      </h1>
-      <p class="text-penito-gray text-base leading-5 mt-5">
-        Purus, eget vitae quam quam id neque. Risus integer pharetra viverra
-        sit. Non, sed consequat morbi sit vitae mollis. Mauris eu diam
-        adipiscing et amet, ut. Donec viverra sed est elit pretium nibh et. Ut
-        varius erat vulputate augue nulla consectetur. Blandit tortor, bibendum
-        mi magna sapien dictum. Ullamcorper ut risus pretium ac aliquam. Risus
-        ante in sit ac pellentesque aliquam ut. Commodo tristique commodo mi
-        malesuada. Sed tristique pharetra tincidunt quis convallis in elementum,
-        aliquam blandit. Purus tortor enim pharetra neque. Quisque sed ultricies
-        fermentum malesuada. Nulla pharetra non pellentesque massa arcu. Feugiat
-        odio sapien faucibus ante sit neque sagittis nibh massa. In commodo nisl
-        mattis et amet diam orci pharetra.
-      </p>
+  <div class="bg-penito-gray-bg py-16 px-4">
+    <div class="max-w-6xl mx-auto flex items-center justify-between gap-5">
+      <div class="max-w-lg md:max-w-md lg:max-w-[594px]">
+        <h1 class="text-[1.75rem] text-penito-black font-semibold">
+          Our Customer Say
+        </h1>
+        <p class="text-penito-gray text-base leading-4 mt-5">
+          Purus, eget vitae quam quam id neque. Risus integer pharetra viverra
+          sit. Non, sed consequat morbi sit vitae mollis. Mauris eu diam
+          adipiscing et amet, ut. Donec viverra sed est elit pretium nibh et. Ut
+          varius erat vulputate augue nulla consectetur. Blandit tortor,
+          bibendum mi magna sapien dictum. Ullamcorper ut risus pretium ac
+          aliquam. Risus ante in sit ac pellentesque aliquam ut. Commodo
+          tristique commodo mi malesuada. Sed tristique pharetra tincidunt quis
+          convallis in elementum, aliquam blandit.
+        </p>
+        <h1 class="text-[#122025] font-semibold mt-4">Oyeniyi Adedoyin</h1>
+        <p class="text-penito-gray text-sm mt-2">Digital Marketer</p>
+      </div>
+      <div class="w-full max-w-md hidden md:block">
+        <img src="@/assets/images/testimonial-room-image.svg" alt="" />
+      </div>
     </div>
   </div>
 </template>

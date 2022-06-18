91eeeb0022cbc38e8b0443821c27b007fbd2ca70
Jesutooni <edeworuyoyou@gmail.com>
2022-06-18 08:00:30 +0100

    footer and footer icons

---
+++ components/base/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="mt-16 py-12 px-4 bg-white max-w-6xl mx-auto">
+    <div class="space-y-8">
+      <div class="flex items-center space-x-2 cursor-pointer">
+        <img src="@/assets/images/icons/penito-icon.svg" alt="" />
+        <h1 class="font-semibold text-2xl lg:text-[2rem] text-penito-black">
+          Penito
+        </h1>
+      </div>
+      
+      <p class="text-penito-gray">Sophisticated simplicity for the independent mind</p>
+
+      <div class="flex gap-4">
+        <img src="@/assets/images/icons/instagram.svg" alt="">
+        <img src="@/assets/images/icons/facebook.svg" alt="">
+        <img src="@/assets/images/icons/linkedin.svg" alt="">
+        <img src="@/assets/images/icons/behance.svg" alt="">
+        <img src="@/assets/images/icons/dribble.svg" alt="">
+      </div>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div class="font-montserrat">
     <baseNavBar />
     <nuxt />
+    <baseFooter />
   </div>
 </template>

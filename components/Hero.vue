a4e043e4e38542e97be8fc983a4bfc12a71b5cf8
Jesutooni <edeworuyoyou@gmail.com>
2022-06-18 14:52:01 +0100

    hero section images

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="max-w-6xl mx-auto">
+  <section class="max-w-6xl mx-auto md:flex items-center py-10 px-4">
     <div class="space-y-11">
-      <h1 class="capitalize text-4xl font-medium">
+      <h1 class="capitalize text-3xl font-medium">
         Discover
         <span class="text-penito-yellow font-bold">The Best</span> Furniture for
         <span class="text-penito-black font-bold"> your Interior</span>
@@ -10,7 +10,7 @@
         We design pieces of furniture and objects that perfectly gap between
         Beauty and functionality
       </p>
-      <div>
+      <div class="flex gap-14 text-center md:text-left">
         <button
           class="px-8 py-4 text-white bg-penito-blue-dark rounded-lg font-semibold text-lg flex items-center gap-3.5"
         >
@@ -45,8 +45,45 @@
           </div>
         </button>
 
-        <div class="w-8 h-8 rounded-full border-l-4 border-penito-yellow"></div>
+        <!-- <div class="w-8 h-8 rounded-full border-l-4 border-penito-yellow"></div> -->
+
+        <button class="flex items-center gap-5 font-semibold text-lg">
+          <div
+            class="w-9 h-9 rounded-full flex justify-center items-center play"
+          >
+            <svg
+              width="14px"
+              height="14px"
+              viewBox="0 0 14 14"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="none"
+                stroke="#000"
+                stroke-width="1.1666666666666667"
+                d="M1.75 12.833L12.25 7L1.75 1.167L1.75 12.833ZM2.917 11.083L10.267 7L2.917 2.917L2.917 11.083ZM4.083 9.333L8.283 7L4.083 4.667L4.083 9.333ZM5.25 7.583L6.3 7L5.25 6.417L5.25 7.583Z"
+              />
+            </svg>
+          </div>
+          Watch Video
+        </button>
+      </div>
+    </div>
+
+    <div class="flex gap-3 w-full">
+      <div class="self-end">
+        <img src="@/assets/images/hero-two.svg" alt="" />
+      </div>
+      <div class="space-y-3">
+        <img src="@/assets/images/hero-one.svg" alt="" />
+        <img src="@/assets/images/hero-three.svg" alt="" />
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+.play {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+}
+</style>

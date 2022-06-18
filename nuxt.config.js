0404550500b78f36f9ace5fead88d09a47ca7a88
Jesutooni <edeworuyoyou@gmail.com>
2022-06-18 21:29:02 +0100

    update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'penito-landing-page',
+    title: 'Penito Furnitures',
     htmlAttrs: {
       lang: 'en'
     },

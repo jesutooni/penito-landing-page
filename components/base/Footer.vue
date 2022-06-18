f53dfb4437547b742986e812db9b88358f09b5a8
Jesutooni <edeworuyoyou@gmail.com>
2022-06-18 09:12:55 +0100

    update footer UI

--- components/base/Footer.vue
+++ components/base/Footer.vue
@@ -1,22 +1,125 @@
 <template>
   <footer class="mt-16 py-12 px-4 bg-white max-w-6xl mx-auto">
-    <div class="space-y-8">
-      <div class="flex items-center space-x-2 cursor-pointer">
-        <img src="@/assets/images/icons/penito-icon.svg" alt="" />
-        <h1 class="font-semibold text-2xl lg:text-[2rem] text-penito-black">
-          Penito
-        </h1>
-      </div>
-      
-      <p class="text-penito-gray">Sophisticated simplicity for the independent mind</p>
+    <div class="flex flex-wrap justify-between">
+      <div class="space-y-8 pr-12 hidden lg:block">
+        <div class="flex items-center space-x-2 cursor-pointer">
+          <img src="@/assets/images/icons/penito-icon.svg" alt="" />
+          <h1 class="font-semibold text-2xl lg:text-[2rem] text-penito-black">
+            Penito
+          </h1>
+        </div>
 
-      <div class="flex gap-4">
-        <img src="@/assets/images/icons/instagram.svg" alt="">
-        <img src="@/assets/images/icons/facebook.svg" alt="">
-        <img src="@/assets/images/icons/linkedin.svg" alt="">
-        <img src="@/assets/images/icons/behance.svg" alt="">
-        <img src="@/assets/images/icons/dribble.svg" alt="">
+        <p class="text-penito-gray font-medium max-w-[280px]">
+          Sophisticated simplicity for the independent mind
+        </p>
+
+        <div class="flex gap-4">
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/instagram.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/facebook.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/linkedin.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/behance.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/dribble.svg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div class="space-y-8 mb-12">
+        <h1 class="font-medium text-lg">Help & Information</h1>
+        <div class="border border-black w-14"></div>
+        <ul class="space-y-3 text-base text-penito-gray">
+          <li class="cursor-pointer">Pagination</li>
+          <li class="cursor-pointer">Terms & Conditions</li>
+          <li class="cursor-pointer">Contact</li>
+          <li class="cursor-pointer">Home page</li>
+          <li class="cursor-pointer">Terms of use</li>
+        </ul>
+      </div>
+
+      <div class="space-y-8 mb-12">
+        <h1 class="font-medium text-lg">About Us</h1>
+        <div class="border border-black w-14"></div>
+        <ul class="space-y-3 text-base text-penito-gray">
+          <li class="cursor-pointer">Help Centre</li>
+          <li class="cursor-pointer">Address Store</li>
+          <li class="cursor-pointer">Privacy Policy</li>
+          <li class="cursor-pointer">Receivers & Amplifiers</li>
+          <li class="cursor-pointer">Clothings</li>
+        </ul>
+      </div>
+
+      <div class="space-y-8 mb-12">
+        <h1 class="font-medium text-lg">Categories</h1>
+        <div class="border border-black w-14"></div>
+        <ul class="space-y-3 text-base text-penito-gray">
+          <li class="cursor-pointer">Delivery</li>
+          <li class="cursor-pointer">Legal Notice</li>
+          <li class="cursor-pointer">Documentation</li>
+          <li class="cursor-pointer">Secure Payment</li>
+          <li class="cursor-pointer">Stores</li>
+        </ul>
       </div>
     </div>
+
+
+
+      <div class="space-y-8 lg:hidden mt-16 max-w-xs mx-auto text-center">
+        <div class="flex items-center justify-center space-x-2 cursor-pointer">
+          <img src="@/assets/images/icons/penito-icon.svg" alt="" />
+          <h1 class="font-semibold text-2xl lg:text-[2rem] text-penito-black">
+            Penito
+          </h1>
+        </div>
+
+        <p class="text-penito-gray font-medium max-w-[280px]">
+          Sophisticated simplicity for the independent mind
+        </p>
+
+        <div class="flex gap-4 justify-center">
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/instagram.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/facebook.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/linkedin.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/behance.svg"
+            alt=""
+          />
+          <img
+            class="cursor-pointer"
+            src="@/assets/images/icons/dribble.svg"
+            alt=""
+          />
+        </div>
+      </div>
   </footer>
 </template>

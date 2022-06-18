4275c1780098062ca079bcef9e949dab6f9ad20c
Jesutooni <edeworuyoyou@gmail.com>
2022-06-18 07:31:51 +0100

    products section responsiveness

--- components/Products.vue
+++ components/Products.vue
@@ -5,12 +5,12 @@
     >
       A Huge Collection of Furniture Products for your Interior
     </h1>
-    <p class="mt-5 text-xl text-penito-gray text-center max-w-[860px] mx-auto">
+    <p class="mt-5 text-lg md:text-xl text-penito-gray text-center max-w-[860px] mx-auto">
       If you have the experience of ten years in an industry, no apostrohe is
       needed. If you have ten years' experience, an apostrophe is needed.
     </p>
 
-    <div class="mt-10 flex justify-center gap-5">
+    <div class="mt-10 hidden lg:flex justify-center gap-5">
       <div
         @click="currentIndex = index"
         v-for="(product, index) in category"
@@ -25,7 +25,74 @@
       </div>
     </div>
 
-    <div class="mt-10"></div>
+    <div class="mt-10 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 lg:gap-7 xl:gap-9">
+      <div
+        v-for="(product, index) in products"
+        :key="index"
+        class="rounded-lg bg-penito-white p-4"
+      >
+        <img
+          class="w-40 h-40 mx-auto"
+          :src="require(`@/assets/images/product-${index + 1}.svg`)"
+          alt=""
+        />
+        <div class="text-sm font-semibold mt-9">
+          <h1 class="text-penito-black">{{ product.name }}</h1>
+          <h1 class="space-x-3.5 mt-1.5">
+            <span class="line-through text-[#A8B6BB]"
+              >${{ product.price }}.00</span
+            ><span class="text-penito-green-light font-bold"
+              >${{ product.discount }}.00</span
+            >
+          </h1>
+          <div class="flex mt-1.5">
+            <svg
+              v-for="n in 4"
+              :key="n"
+              width="12px"
+              height="12px"
+              viewBox="0 0 12 12"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="#FFC95E"
+                fill-rule="evenodd"
+                points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"
+                d="M6 8.334L2.5 11L4 7L1 4.75L4.75 4.75L6 1L7.25 4.75L11 4.75L8 7L9.5 11Z"
+              />
+            </svg>
+            <svg
+              width="12px"
+              height="12px"
+              viewBox="0 0 12 12"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <path
+                  fill="#000"
+                  fill-opacity=".2"
+                  points="12 16.667 12 2 14.5 9.5 22 9.5 16 14 19 22"
+                  d="M6 8.334L6 1L7.25 4.75L11 4.75L8 7L9.5 11Z"
+                />
+                <path
+                  fill="#FFC95E"
+                  points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2"
+                  d="M6 8.334L2.5 11L4 7L1 4.75L4.75 4.75L6 1Z"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center">
+      <button
+        class="mt-9 text-penito-white bg-penito-blue-dark py-2.5 px-8 rounded-full"
+      >
+        See more
+      </button>
+    </div>
   </section>
 </template>
 
@@ -64,6 +131,11 @@ export default {
           name: "Plastic Chair",
           price: "180",
           discount: "130",
+        },
+        {
+          name: "Wooden Room Light",
+          price: "185",
+          discount: "120",
         },
         {
           name: "Couch Sofa",
